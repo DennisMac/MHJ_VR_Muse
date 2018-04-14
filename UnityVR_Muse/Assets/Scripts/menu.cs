f2b6748246d7d9b740b59dcf2af5ee47c1e89b24
natmaxex <nathanrumsey@gmail.com>
2018-04-14 14:00:00 -0400

    doing it again

---
+++ UnityVR_Muse/Assets/Scripts/menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //PlayerPrefs.SetInt("with or without sound", 20);//with or without sound
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    //vocal guide lead, mobal guide lead
+    void VocalG() {
+
+    }
+
+    void MobalG() {
+
+    }
+}
